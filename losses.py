9b08420d3ff32bae40691a2ecc73a8dae473d613
bomtorazek <bomtorazek@kaist.ac.kr>
2021-06-17 06:47:33 +0000

    comment on loss

--- losses.py
+++ losses.py
@@ -42,19 +42,19 @@ class SupConLoss(nn.Module):
             features = features.view(features.shape[0], features.shape[1], -1)
 
         batch_size = features.shape[0]
-        if labels is not None and mask is not None: # 1 1
+        if labels is not None and mask is not None: # 둘 다 있을 때
             raise ValueError('Cannot define both `labels` and `mask`')
-        elif labels is None and mask is None: # 0 0, unsupervised
+        elif labels is None and mask is None: # 둘 다 없을 때, unsupervised loss (SimCLR loss임)
             mask = torch.eye(batch_size, dtype=torch.float32).to(device)
-        elif labels is not None: # 1 0 
+        elif labels is not None: # label이 있으면 그걸 기반으로 mask를 만든다.
             labels = labels.contiguous().view(-1, 1)
             if labels.shape[0] != batch_size:
                 raise ValueError('Num of labels does not match num of features')
             mask = torch.eq(labels, labels.T).float().to(device)
-        else: # 0 1
+        else: # 마스크가 주어질 수도 있다.
             mask = mask.float().to(device)
 
-        contrast_count = features.shape[1]
+        contrast_count = features.shape[1] # view를 말함. 한 이미지에서 crop으로 2 view
         contrast_feature = torch.cat(torch.unbind(features, dim=1), dim=0)
         if self.contrast_mode == 'one':
             anchor_feature = features[:, 0]
@@ -68,10 +68,12 @@ class SupConLoss(nn.Module):
         # compute logits
         anchor_dot_contrast = torch.div(
             torch.matmul(anchor_feature, contrast_feature.T),
-            self.temperature)
-        # for numerical stability
+            self.temperature) # 논문 상의 zi*zp matrix 만듦.
+
+        # for numerical stability,  want to avoid very large logits that lead to NaN problem
         logits_max, _ = torch.max(anchor_dot_contrast, dim=1, keepdim=True)
         logits = anchor_dot_contrast - logits_max.detach()
+        #graident는 안 변함.
 
         # tile mask
         mask = mask.repeat(anchor_count, contrast_count)
@@ -82,14 +84,18 @@ class SupConLoss(nn.Module):
             torch.arange(batch_size * anchor_count).view(-1, 1).to(device),
             0
         )
+        # 그냥 ones에서 diagonal만 0으로 바꾸는 코드인데 어렵게 짬.
+
         mask = mask * logits_mask
 
         # compute log_prob
-        exp_logits = torch.exp(logits) * logits_mask
+        exp_logits = torch.exp(logits) * logits_mask # logits_mask는 자기 자신만 제외함.
         log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True))
+            # eq(2) 분모                eq(2) 분자
 
         # compute mean of log-likelihood over positive
         mean_log_prob_pos = (mask * log_prob).sum(1) / mask.sum(1)
+        # mask를 이용해 positive pair에 대해서 계산
 
         # loss
         loss = - (self.temperature / self.base_temperature) * mean_log_prob_pos
